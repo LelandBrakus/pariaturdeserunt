6c3cd3468d0ea3b319b869c354cb26d2b91779c2
LelandBrakus <IdJSWSpy502937@outlook.com>
2024-03-31 21:21:50 +0800

    consectetur

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// export main entry and finish functions
+export { default as gotenberg } from './gotenberg'
+export { default as please } from './please'
+
+// export conversion functions
+export { default as markdown } from './markdown'
+export { default as convert } from './convert'
+export { default as office } from './office'
+export { default as merge } from './merge'
+export { default as ping } from './ping'
+export { default as html } from './html'
+export { default as url } from './url'
+
+// export modifiers functions and constants
+export { default as set } from './set'
+export { default as to } from './to'
+export * from './set-helpers'
+export * from './to-helpers'
+export * from './page'
+
+// export pipe helper
+export { default as pipe } from './_pipe'
+
+// export types
+export * from './_types'
